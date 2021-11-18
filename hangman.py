a40df21371a43e97e33df708ec2504087b997b9c
npicciano79 <npicciano79@gmail.com>
2021-11-17 20:59:07 -0500

    hangman.py

--- hangman.py
+++ hangman.py
@@ -1,20 +1,35 @@
 #hangman game
 
 def word_enter():
+    word=''
     bad_char={"/",".",",","[","]",")","(","&","%"}
+    bad_key="False"
     word=input("Enter your word or phrase: ")
-    if word != "":
-        for i in bad_char:
-            if i not in word:
-                continue
-            else:
-                word_enter()   
+    for i in word:
+        if i in bad_char:
+            bad_key="True"
+
+    if word == "" or bad_key=="True":   
+        print("Word is invalid: ")
+        word_enter()
+    else:
         word_guess(word)
-    #print("Word entered is invalid.")
-    #word_enter()
 
 def word_guess(word):
-    print(f"the word entered is {word}")
+    chance=0
+    correct=0
+    guess=input(print("Guess a letter: "))
+    word_len=len(word)
+
+    for i in word:
+        if guess==i:
+            correct+=1
+            print("The letter "+ guess+" is correct "+correct)
+        else:
+            print("false")
+
+
+    
 
 
 
